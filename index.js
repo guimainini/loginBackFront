59ca70b45d3a30ccb57f768f9d180f21afdd92e0
Guillermo Mainini <guimainini@gmail.com>
2021-03-02 23:12:18 -0300

    instalacion de dependencias y configuracion del servidor express

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 
+const express = require ('express');
 
-console.log('Hola desde Nodeeeeeeeeeee');
+// Crear el servidor/aplicacion de express
+const app = express();
+
+
+// peticion GET
+app.get('/', ( req, res ) => {
+
+    console.log('Peticion en el /');
+
+    res.json({
+        ok  : true,
+        msg : 'Todo salio bien',
+        uid : 1234
+    })
+
+});
+
+
+
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
+
+
+
+
 
 
